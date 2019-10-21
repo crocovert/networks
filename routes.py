5aa536f06fb2a5dad105f0d28ca561c4817bbf75
crocovert <patrick.palmier@cerema.fr>
2019-10-21 17:06:03 +0200

    2.3.16
    fix bug in routes.txt

--- routes.py
+++ routes.py
@@ -172,7 +172,7 @@ class Routes(QgsProcessingAlgorithm):
                         liste=[id_iti]
                         liste.extend(tij[d][i] for i in [1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,20,21])
                         for t in [3,4,5,6,7,8,9,10,11,12,13,14,15,18]:
-                            liste[t]=float(liste[t])
+                            liste[t]=float(liste[t].replace(',','.'))
                         liste=liste[1:22]
                         f.setAttributes(liste)
                         
